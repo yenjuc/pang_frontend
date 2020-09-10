0a1baecc353db566240d7459b09a926b25d1d023
yenjuc <yenjuc@users.noreply.github.com>
2020-09-10 13:53:34 +0800

    增加用户列表显示组件

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -10,30 +10,14 @@
     </ul>
 
     <!--v-for item in list...-->
-    <!--child component-->
     <div class="list-group">
-      <a v-for="index of 30" :key="index" v-show="index > (page-1)*10 && index <= page*10" class="list-group-item" style="display: flex">
-        <div class="info_block">
-          <h4 class="list-group-item-heading">Username {{index}}</h4>
-          <p class="list-group-item-text">@email</p>
-        </div>
-        <!--div class="dropdown open">
-          <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-          设定用户类型
-          <span class="caret"></span>
-          </button>
-          <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-            <li><a href="#">普通用户</a></li>
-            <li><a href="#">设备提供者</a></li>
-          </ul>
-        </div-->
-        <button type="button" class="btn btn-default">
-          <i class="fas fa-edit"></i>
-        </button>
-        <button type="button" class="btn btn-default">
-          <i class="fas fa-trash-alt"></i>
-        </button>
-      </a>
+      <div class="userslist" v-if="type === 'users'">
+        <!-- v-for="(item,index) in lists" v-show="index > (page-1)*10 && index <= page*10" class="list-group-item"-->
+        <a v-for="index of 30" :key="index" v-show="index > (page-1)*10 && index <= page*10" class="list-group-item">
+          <!--user-info username=item.username email=item.email></username-->
+          <user-info username="test name" email="test email"></user-info>
+        </a>
+      </div>
     </div>
 
     <nav aria-label="Page navigation">
@@ -60,6 +44,7 @@
 </template>
 
 <script>
+import UserInfo from './UserInfo'
 export default {
   data () {
     return {
@@ -67,6 +52,9 @@ export default {
       page: 1,
       list: []
     }
+  },
+  components: {
+    'user-info': UserInfo
   }
 }
 </script>>
@@ -79,15 +67,5 @@ export default {
   border-radius: 10px;
   box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
   width: 75%;
-}
-
-.info_block{
-  width: 90%;
-  padding: 10px;
-  text-align: left;
-}
-
-.btn-default{
-  margin: 0 auto;
 }
 </style>
---
+++ src/components/UserInfo.vue
@@ -0,0 +1,45 @@
+<template>
+  <!--v-for item in list...-->
+  <div class="user" style="display: flex">
+      <div class="info_block">
+        <h4 class="list-group-item-heading">{{username}}</h4>
+        <p class="list-group-item-text">{{email}}</p>
+      </div>
+      <!--div class="dropdown open">
+          <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+          设定用户类型
+          <span class="caret"></span>
+          </button>
+          <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+            <li><a href="#">普通用户</a></li>
+            <li><a href="#">设备提供者</a></li>
+          </ul>
+        </div-->
+      <button type="button" class="btn btn-default">
+        <i class="fas fa-edit"></i>
+      </button>
+      <button type="button" class="btn btn-default">
+        <i class="fas fa-trash-alt"></i>
+      </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['username', 'email']
+}
+</script>>
+
+<style scoped>
+
+.info_block{
+  width: 90%;
+  padding: 10px;
+  text-align: left;
+}
+
+.btn-default{
+  margin: 0 5px;
+  width: 42px;
+}
+</style>
