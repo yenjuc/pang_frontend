f90135c09fbfaced36b91af9d13cd1d485cf16da
Diana_pwf <Diana_pwf@163.com>
2020-09-10 22:30:13 +0800

    加入CommonUser中用户查看所有上架设备的列表展示

--- src/components/DeviceInfo.vue
+++ src/components/DeviceInfo.vue
@@ -4,12 +4,16 @@
         <h4 class="list-group-item-heading">{{device_name}}</h4>
         <div style="display: inline-block">
           <div class="detailedinfo">
+            <i class="fas fa-clock"></i>
+            <span class="list-group-item-text">{{device_info}}</span>
+          </div>
+          <div class="detailedinfo">
             <i class="fas fa-map-marker-alt"></i>
             <span class="list-group-item-text">{{device_address}}</span>
           </div>
           <div class="detailedinfo">
-            <i class="fas fa-clock"></i>
-            <span class="list-group-item-text">{{device_timeout}}</span>
+            <i class="fas fa-address-book"></i>
+            <span class="list-group-item-text">{{device_owner}}</span>
           </div>
           <div class="detailedinfo">
             <i class="fas fa-address-book"></i>
@@ -22,7 +26,7 @@
 
 <script>
 export default {
-  props: ['device_name', 'device_address', 'device_timeout', 'device_contact']
+  props: ['device_name', 'device_address', 'device_info', 'device_owner', 'device_contact']
 }
 </script>>
 
